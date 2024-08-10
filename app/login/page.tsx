23748e1de038cb8e29be4774cc4da78701e72885
Daniil <bkom01@icloud.com>
2024-08-11 02:09:35 +0300

    ci/cd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 import { Login } from "@/screens/Login";
 
 export default function LoginPage() {
-  const botName = process.env.NEXT_PUBLLIC_BOT_NAME;
-  return <Login botName={botName || ""} />;
+  const botName = process.env.NEXT_PUBLIC_BOT_NAME;
+  return <Login botName={botName!} />;
 }
